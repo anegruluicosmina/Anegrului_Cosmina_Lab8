581bc73c0bf2ca0b1daa1e80e8bb9e2045500db6
MARIA-COSMINA ANEGRULUI <maria.anegrului@stud.ubbcluj.ro>
2020-11-26 18:03:22 +0200

    Add project files.

---
+++ Anegrului_Cosmina_Lab8/Data/Anegrului_Cosmina_Lab8Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Anegrului_Cosmina_Lab8.Models;
+
+namespace Anegrului_Cosmina_Lab8.Data
+{
+    public class Anegrului_Cosmina_Lab8Context : DbContext
+    {
+        public Anegrului_Cosmina_Lab8Context (DbContextOptions<Anegrului_Cosmina_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Anegrului_Cosmina_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Anegrului_Cosmina_Lab8.Models.Publisher> Publisher { get; set; }
+    }
+}
---
+++ Anegrului_Cosmina_Lab8/Migrations/20201126151250_BookCategory.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Anegrului_Cosmina_Lab8.Migrations
+{
+    public partial class BookCategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Book",
+                type: "decimal(6,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(6,2");
+
+            migrationBuilder.AddColumn<int>(
+                name: "PublisherID",
+                table: "Book",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "BookCategory",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryName = table.Column<string>(nullable: true),
+                    BookCategoryID = table.Column<int>(nullable: true),
+                    BookID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookCategory", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_BookCategory_BookCategory_BookCategoryID",
+                        column: x => x.BookCategoryID,
+                        principalTable: "BookCategory",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_BookCategory_Book_BookID",
+                        column: x => x.BookID,
+                        principalTable: "Book",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Publisher",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PublisherName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Publisher", x => x.ID);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Book_PublisherID",
+                table: "Book",
+                column: "PublisherID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookCategory_BookCategoryID",
+                table: "BookCategory",
+                column: "BookCategoryID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookCategory_BookID",
+                table: "BookCategory",
+                column: "BookID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Book_Publisher_PublisherID",
+                table: "Book",
+                column: "PublisherID",
+                principalTable: "Publisher",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Book_Publisher_PublisherID",
+                table: "Book");
+
+            migrationBuilder.DropTable(
+                name: "BookCategory");
+
+            migrationBuilder.DropTable(
+                name: "Publisher");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Book_PublisherID",
+                table: "Book");
+
+            migrationBuilder.DropColumn(
+                name: "PublisherID",
+                table: "Book");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Book",
+                type: "decimal(6,2",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(6,2)");
+        }
+    }
+}
